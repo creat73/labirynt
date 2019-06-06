630589d4f5a4c629447ad75fedebac9125f1dffc
creat73 <nikill1125@gmail.com>
2019-06-06 21:15:55 +0200

    Add random numbers generator for matrix

--- labirynt.cpp
+++ labirynt.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-#include <math.h>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 
 int main()
 {
 	int n;
+	srand(time(0));
+	
 	cout<<"Podaj wymiary tablicy: ";
 	cin>>n;
 	int arr[n][n];
 	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			//Random number from -100 to 100
+			arr[i][j] =-100 + (rand()%200)+1; 
+		}
+	}
 }
